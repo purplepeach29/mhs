f4d05abb3f0db0c46dc2b5593ead445f8e18a667
purplepeach29 <prachiraghav.pr@gmail.com>
2024-12-05 19:02:37 +0530

    Add onboarding page, custombutton, tabs

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,50 @@
+
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
+import { Redirect, router } from 'expo-router';
 import{ Link}  from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { images }from '../constants';
+import CustomButton from '../components/CustomButton';
 export default function App() {
   return (
-    // <SafeAreaView>
-    <View className="flex-1 items-center justify-center bg-blue">
-      <Text className="text-3xl font-pblack">MHS</Text>
-      <Link href="/home" style={{ color: 'blue'}}>Go to home</Link>
-      <StatusBar style="auto" />
-    </View>
-    // </SafeAreaView>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height:'100%'}}>
+        <View className="w-full flex flex-col items-center justify-center min-h-[85vh] px-4">
+          <Image 
+            source={images.logo}
+            className="w-[130px] h-[80px]"
+            resizeMode='contain'
+          />
+          <Image 
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode='contain'
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold 
+            text-center">Explore Endless Possibilities with{' '}
+              <Text className="text-secondary-200">MHS</Text>
+            </Text>
+            <Image 
+            source={images.path}
+            className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+            resizeMode='contain'
+          />
+          </View>
+          <Text  className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with MHS
+          </Text>
+          <CustomButton 
+            title="Continue with Email"
+            handlePress={()=> router.push('/sign-in')}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor='#161622'
+            style='light'/>
+    </SafeAreaView>
   );
 }
