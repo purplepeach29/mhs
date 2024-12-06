10d0be623dce015625bd6820784e498ed35c2c29
purplepeach29 <prachiraghav.pr@gmail.com>
2024-12-06 13:52:12 +0530

    Add FormField, sign-in, sign-up

---
+++ components/FormField.jsx
@@ -0,0 +1,34 @@
+import { View, Text, TextInput, Touchable, TouchableOpacity, Image } from 'react-native'
+import { useState } from 'react'
+import { icons } from "../constants"
+
+const FormField = ({title, value, placeholder, handleChangeText, otherStyles, ...props}) => {
+  const [showPassword, setShowPassword] = useState(false)
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text text-gray-100 font-pmedium">{title}</Text>
+      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl
+            focus:border-secondary items-center flex-row">
+        <TextInput 
+          className="text-white flex-1 font-psemibold text-base"
+          value={value}
+          onChangeText={handleChangeText}
+          placeholder={placeholder}
+          placeholderTextColor="#7b7b8b"
+          secureTextEntry={title==='Password' && !showPassword}
+        />
+        {title === 'Password' && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Image 
+              source={!showPassword ? icons.eye : icons.eyeHide} 
+              className="w-6 h-6"  
+              resizeMode='contain'
+              />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  )
+}
+
+export default FormField
